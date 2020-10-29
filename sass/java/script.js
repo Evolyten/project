51152d81ea2bea19a7a4c3f1ef02ca4137dcb733
Ivan <example@gmail.com>
2020-10-29 21:09:34 +0200

    first commit

---
+++ sass/java/script.js
@@ -0,0 +1,72 @@
+// var name ="Ivan";
+// let number = 7;
+// const pi = 3.14; 
+
+// let leftBorderWidth = 200;
+
+
+//number
+//string - "", '',``
+//true/false
+//null
+//undefined
+
+//let obj = {
+//	name: 'apple',
+//	color: 'green',
+//	weight: 200
+
+//}
+//Symbol
+
+
+//alert(1234)
+//console.log(number);
+//let answer = prompt("Вам есть 18?", "");
+//console.log(answer);
+//console.log(4+4)\
+
+
+//let isChecked =true,
+//	isClose = false;
+
+//	console.log(isChecked && isClose);
+
+//		console.log(isChecked || isClose);
+
+//if (2*6 == 8*1) {
+//	console.log('Verno')
+//	console.log('Mistake')
+//}
+
+
+//let answer = confirm("Вам есть 18?");
+
+//if(answer){
+//	console.log('Проходите')
+//} else {
+//	console.log('Go away')
+//}
+
+// const num =50;
+// if(num<49){
+// 	console.log('No')
+// } else if (num>100) {
+// 	console.log("a lot")
+// } else {
+// 	console.log('Verno')
+// }
+
+// for (let i =1; i<8; i++) {
+// 	console.log(i)
+// }
+
+
+// function logging(a, b){
+// 	console.log(a + b)
+// }
+
+
+// logging(3, 5);
+
+console.log('a'>'1')
